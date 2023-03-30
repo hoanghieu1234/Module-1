2bc1670dbefd505d0ece3b6305797b97ef8e8b9f
nguyễn hoàng hiếu <77264798+hoanghieu1234@users.noreply.github.com>
2023-03-30 09:21:35 +0700

    up

---
+++ script.js
@@ -0,0 +1,4 @@
+let value  = prompt('Nhập vào tuổi của các bạn:')
+console.log('Tuổi nhập vào là :',value);
+console.log(('Nhập năm sinh của bạn:',prompt('Nhập năm sinh:')));
+console.log('Bạn đồng ý làm người yêu tôi ko',confirm('chọn đi'));
